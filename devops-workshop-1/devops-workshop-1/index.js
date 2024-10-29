ccd879d76f48bc29086cde6c4bcc290f5b1517d0
Jorge Angel <jorgeangel.vazquezfernandez@usp.ceu.es>
2024-10-29 15:27:46 +0100

    Primer commit: Añadir código base

--- devops-workshop-1/devops-workshop-1/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./server');
-const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
-
-module.exports = server; // Exporta el servidor
